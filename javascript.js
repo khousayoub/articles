b29ccbcde305842ff225ae1b6c14b4c4152c13ea
khousayoub <khous.ayoub@gmail.com>
2016-10-26 16:49:25 +0200

    save

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
   function remove(x) {
       parent = document.getElementById("container");
-      parent.removeChild(document.getElementById(x));
+      parent.removeChild(document.getElementById("container:nth-child(".x.")"));
   }
